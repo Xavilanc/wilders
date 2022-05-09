92088b57518f0bfa2e50780a4fca95229f150871
LANCELLE Xavier (SNCF VOYAGEURS / DIRECTION DE LIGNES ALPES / Siège UO Servi CHAMBERY) <xavilanc@gmail.com>
2022-05-09 14:17:05 +0200

    first commit

---
+++ node.js
@@ -0,0 +1,6 @@
+const wilders = ["Naim", "Lisa", "Laurent", "Xavier"];
+import chalk from "chalk";
+console.log(chalk.red("Hello " + wilders[0]));
+console.log(chalk.blue("Hello " + wilders[1]));
+console.log(chalk.yellow("Hello " + wilders[2]));
+console.log(chalk.green("Hello " + wilders[3]));
